3274718d06ee4c4caf6f68120e76fe8a20da156c
峰 <leelapfung@gmail.com>
2018-08-11 03:24:04 +0800

    build..

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-// importScripts('/cache-polyfill.js');
-
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
@@ -12,3 +10,9 @@ self.addEventListener('install', function(e) {
    })
  );
 });
+
+self.addEventListener('fetch',
+  function(event) {
+    console.log(event.request.url);
+  }
+);
