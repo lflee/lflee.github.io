9c6a0969a72908cf65a724d9358c24569303e591
峰 <leelapfung@gmail.com>
2018-08-10 22:28:07 +0800

    sw

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-console.log('Hello from sw.js');
+// importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('f').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/styles/main.css'
+     ]);
+   })
+ );
+});
